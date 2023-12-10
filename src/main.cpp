49ab8aa18fcf465e576b1f97495b33ef5eb44440
tichise <43707+tichise@users.noreply.github.com>
2023-12-10 12:38:35 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-#include "BMM150Compass.h"
 
 // SDAとSCLのピン番号を定義（あなたのハードウェア設定に合わせて変更してください）
 #define PIN_SDA 13
@@ -7,6 +6,7 @@
 
 // BMM150Compassのインスタンスを作成
 BMM150Compass compass(PIN_SDA, PIN_SCL);
+#include "BMM150Compass/BMM150Compass.h"
 
 void setup()
 {
